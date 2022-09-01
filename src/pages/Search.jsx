fde59ede4716ae30da5003760ed79598adb5b70a
Raphael Pacheco <raphaelrogen@gmail.com>
2022-08-31 22:47:11 -0300

    requisito 05 completo

--- src/pages/Search.jsx
+++ src/pages/Search.jsx
@@ -2,13 +2,42 @@ import React, { Component } from 'react';
 import Header from './Header';
 
 class Search extends Component {
+  state = {
+    textNumber: '',
+  };
+
+  handleChange = ({ target }) => {
+    const { value } = target;
+    this.setState({
+      textNumber: value,
+    });
+  };
+
   render() {
+    const { textNumber } = this.state;
     return (
       <div data-testid="page-search">
         <Header />
         <h1>
           Search
         </h1>
+        <form>
+          <input
+            data-testid="search-artist-input"
+            type="text"
+            value={ textNumber }
+            onChange={ this.handleChange }
+
+          />
+          <button
+            type="button"
+            data-testid="search-artist-button"
+            disabled={ textNumber.length < 2 }
+          >
+            Pesquise
+          </button>
+
+        </form>
       </div>
     );
   }
