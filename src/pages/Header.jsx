6d78e9f84eddf10f11a31eb3a65cd26b4d18ab1d
Raphael Pacheco <raphaelrogen@gmail.com>
2022-08-31 21:04:06 -0300

    requisito 04 pronto

--- src/pages/Header.jsx
+++ src/pages/Header.jsx
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 import { getUser } from '../services/userAPI';
 
 class Header extends Component {
@@ -20,6 +21,9 @@ class Header extends Component {
     const { userLog, loaded } = this.state;
     return (
       <header data-testid="header-component">
+        <Link to="/search" data-testid="link-to-search">Search</Link>
+        <Link to="/favorites" data-testid="link-to-favorites">Favorites</Link>
+        <Link to="/profile" data-testid="link-to-profile">profile</Link>
         <h1>Cabeçalho</h1>
         { loaded ? <p>Carregando...</p>
           : <p data-testid="header-user-name">{userLog}</p> }
